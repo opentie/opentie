053a4a1d1dcbeb38683631ea0e107ca1a72536ef
everysick <s.wakeup31@gmail.com>
2016-04-07 01:11:38 +0900

    Change default session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_opentie_session'
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_opentie-app_session',
+  expire_after: 30.minutes
+)
