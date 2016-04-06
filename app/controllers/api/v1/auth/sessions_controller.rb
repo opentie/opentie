48df9a14f3293022ef80c816b72e5523205fc625
everysick <s.wakeup31@gmail.com>
2016-04-07 01:12:35 +0900

    Impliment sign_in and sign_out logic

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,3 +1,21 @@
 class Api::V1::Auth::SessionsController < ApplicationController
 
+  before_action :authenticate_user!, only: :sign_out
+
+  def sign_in
+    password = params[:password]
+    email = params[:email]
+
+    account = Account.find_by(email: email)
+
+    render_unauthorized unless account.authenticate(password)
+    sign_in!(account)
+
+    render json: { status: 200 }
+  end
+
+  def sign_out
+    sign_out!
+    render json: { status: 200 }
+  end
 end
