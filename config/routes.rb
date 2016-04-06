5f9c9a36a50fb0cdc7ba0295d7ac75f8fc0034f1
everysick <s.wakeup31@gmail.com>
2016-04-06 15:37:38 +0900

    Simplify application routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,61 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :groups do
-        resources :sub_schemata do
-          resource :request
+      resources :groups,
+        only: [:show, :edit, :new, :create, :update] do
+
+        resources :sub_schemata,
+          controller: 'groups/sub_shcemata',
+          only: [:index, :show] do
+
+          resource :request,
+            controller: 'groups/sub_schemata/request',
+            only: [:show, :new, :edit, :create, :update, :destroy]
         end
 
-        resources :topics do
-          resources :posts
+        resources :topics,
+          controller: 'groups/topics',
+          only: [:index, :show, :new, :edit, :create, :update] do
+
+          resource :post,
+            controller: 'groups/topics/post',
+            only: [:new, :edit, :create, :update]
         end
       end
 
-      resources :divisions do
-        resources :group_schemata do
-          resources :groups do
-            resources :change_logs, only: :index
+      resources :divisions,
+        only: [:show, :edit, :new, :create, :update] do
+        resources :group_schemata,
+          controller: 'divisions/group_schemata',
+          only: [:index, :show, :new, :edit, :create, :update] do
+
+          resources :groups,
+            controller: 'divisions/group_schemata/groups',
+            only: [:index, :show, :edit, :update] do
+
+            resources :change_logs,
+              controller: 'divisions/group_schemata/groups/change_logs',
+              only: [:index]
           end
 
-          resources :sub_schemata
+          resources :sub_schemata,
+            controller: 'divisions/group_schemata/sub_schemata',
+            only: [:index, :show, :new, :edit, :create, :update, :destroy]
         end
 
-        namespace :message do
-          resources :topics do
-            resources :posts
+        namespace :message,
+          path: 'message' do
+          resources :topics,
+            only: [:index, :show, :new, :edit, :create, :update] do
+
+            resource :post,
+              controller: 'topics/post',
+              only: [:new, :edit, :create, :udpate]
           end
 
-          resources :labels
+          resources :labels,
+            controller: 'labels',
+            only: [:index, :new, :edit, :update, :create, :destroy]
         end
       end
     end
