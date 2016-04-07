d8ab6cee3cdde6a799942e76d0839e75ed7b8614
everysick <s.wakeup31@gmail.com>
2016-04-07 15:23:22 +0900

    Impliment password reset logic with FIXME

--- app/controllers/api/v1/account/password_controller.rb
+++ app/controllers/api/v1/account/password_controller.rb
@@ -1,3 +1,32 @@
 class Api::V1::Account::PasswordController < ApplicationController
 
+  before_action :authenticate_account!, only: :update
+
+  # Request that reset password
+  def create
+    email = params[:email]
+    account = Account.find_by(email: email)
+
+    # Fetch from modngodb # FIXME
+    name = "sample_name"
+
+    token = PasswordRecoveryToken.create_new_token(account.id)
+
+    AccountMailer.reset_password(email, name, token).deliver
+  end
+
+  # Update password
+  def update
+    token = params[:token]
+    new_password = params[:password]
+
+    recovery_token =
+      current_account.password_recovery_tokens.find_by!(token: token)
+
+    current_account.update!(password: new_password)
+
+    recovery_token.disable
+
+    render json: { result: true }
+  end
 end
