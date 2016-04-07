10ffb50568ff365c8e37fe95de02781eae6eb0d9
everysick <s.wakeup31@gmail.com>
2016-04-07 15:22:57 +0900

    Impliment mail form when reset_password

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,3 +1,13 @@
 class AccountMailer < ApplicationMailer
   layout 'basic'
+
+  def reset_password(email, name, token)
+    @account_name = name
+    @account_email = email
+
+    @project_name = "opentie" # FIXME
+    @confirm_url = "/account/password_reset_form?token=#{token}" # FIXME
+
+    mail to: email, subject: "#{@project_name}のパスワードリセット"
+  end
 end
