cdcb2eeed0ab3850b3e53f869de4d4dfabfb4a39
everysick <s.wakeup31@gmail.com>
2016-04-07 15:19:33 +0900

    Fix method that create new token

--- app/models/email_recovery_token.rb
+++ app/models/email_recovery_token.rb
@@ -2,8 +2,9 @@ class EmailRecoveryToken < RecoveryToken
 
   def self.create_new_token(account_id, email)
     raise ActiveRecord::RecordInvalid if email.empty?
-    token = new(token: generate_token, account_id: account_id, substitute: email)
-    token.save!
+    recovery_token = new(token: generate_token, account_id: account_id, substitute: email)
+    recovery_token.save!
+    recovery_token.token
   end
 
   def recovery_email
--- app/models/password_recovery_token.rb
+++ app/models/password_recovery_token.rb
@@ -1,7 +1,8 @@
 class PasswordRecoveryToken < RecoveryToken
 
   def self.create_new_token(account_id)
-    token = new(token: generate_token, account_id: account_id)
-    token.save!
+    recovery_token = new(token: generate_token, account_id: account_id)
+    recovery_token.save!
+    recovery_token.token
   end
 end
