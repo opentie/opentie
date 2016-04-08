8f6e61f7e79cee9d0982371d11207e3a5faf3342
everysick <s.wakeup31@gmail.com>
2016-04-08 23:43:25 +0900

    Add admin flag

--- db/migrate/20160405081617_create_account.rb
+++ db/migrate/20160405081617_create_account.rb
@@ -1,10 +1,11 @@
 class CreateAccount < ActiveRecord::Migration
   def change
     create_table :accounts, id: :uuid do |t|
-      t.integer :kibokan_id,            null: false
-      t.string  :email,                              default: nil
-      t.string  :password_digest,       null: false, default: ""
-      t.timestamps                      null: false
+      t.integer  :kibokan_id,            null: false
+      t.string   :email,                              default: nil
+      t.string   :password_digest,       null: false, default: ""
+      t.boolean  :is_admin,                           default: false
+      t.timestamps                       null: false
     end
 
     add_index :accounts, :kibokan_id
