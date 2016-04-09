5dad07a04f281099e68d1b99f83e70a53a9ea57d
everysick <s.wakeup31@gmail.com>
2016-04-09 16:57:29 +0900

    Create spec that test account_mailer

--- spec/mailers/account_mailer_spec.rb
+++ spec/mailers/account_mailer_spec.rb
@@ -1,5 +1,67 @@
 require "rails_helper"
 
 RSpec.describe AccountMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @mail_to = 'opentie@example.com'
+    @account_name = 'account_name'
+    @mail_from = "do-not-reply.#{ Rails.application.config.global_config.organization_mail}"
+    @sample_token = 'sample_token'
+    @service_name = Rails.application.config.global_config.service_name
+  end
+
+  describe 'AccountMailer#reset_password' do
+    let(:mail) do
+      AccountMailer.reset_password(
+        @mail_to,
+        @account_name,
+        @sample_token
+      )
+    end
+
+    it 'send an email' do
+      expect do
+        mail.deliver_now
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq "#{@service_name}のパスワードリセット"
+    end
+
+    it "renders the receiver email" do
+      expect(mail.to).to eq [@mail_to]
+    end
+
+    it "renders the sender email" do
+      expect(mail.from).to eq [@mail_from]
+    end
+  end
+
+  describe 'AccountMailer#regist_email' do
+    let(:mail) do
+      AccountMailer.regist_email(
+        @mail_to,
+        @account_name,
+        @sample_token
+      )
+    end
+
+    it 'send an email' do
+      expect do
+        mail.deliver_now
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq "#{@service_name}へこのメールアドレスでアカウント登録されました"
+    end
+
+    it "renders the receiver email" do
+      expect(mail.to).to eq [@mail_to]
+    end
+
+    it "renders the sender email" do
+      expect(mail.from).to eq [@mail_from]
+    end
+  end
 end
