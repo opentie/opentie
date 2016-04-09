9272f7c7bcd7bf46e78ff6e660e3c6c038af7b56
everysick <s.wakeup31@gmail.com>
2016-04-09 19:13:41 +0900

    Create spec that test models and write email_recovery_token spec

---
+++ spec/models/division_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Division, type: :models do
+
+end
---
+++ spec/models/email_recovery_token_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe EmailRecoveryToken, type: :models do
+  before do
+    @new_email = "new@email.com"
+  end
+
+  describe "Change email flow" do
+    let(:account) { FactoryGirl.create(:account) }
+
+    it "create change email token" do
+      expect do
+        @token = EmailRecoveryToken.create_new_token(account, @new_email)
+      end.to change { account.email_recovery_tokens.count }.by(1)
+    end
+
+    it "change email" do
+      token = EmailRecoveryToken.create_new_token(account, @new_email)
+      account.update_email_with_recovery_token(token)
+      expect(account.email).to eq(@new_email)
+    end
+  end
+end
