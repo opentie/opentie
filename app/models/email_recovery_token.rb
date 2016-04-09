07aaca98f0b215d60fe698a15cc39173eea8a5e8
everysick <s.wakeup31@gmail.com>
2016-04-09 19:12:30 +0900

    Fix typo

--- app/models/email_recovery_token.rb
+++ app/models/email_recovery_token.rb
@@ -1,6 +1,6 @@
 class EmailRecoveryToken < RecoveryToken
 
-  def self.create_new_token(account_id, email)
+  def self.create_new_token(account, email)
     raise ActiveRecord::RecordInvalid if email.empty?
     recovery_token = new(token: generate_token, account: account, substitute: email)
 
