a1e7e3a02ec541f014a700d9a0361e20caed2b4e
everysick <s.wakeup31@gmail.com>
2016-04-09 16:32:33 +0900

    Change setting to execute guard

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Opentie
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.web_console.development_only = false
+
     # Application timezone
     config.time_zone = 'Tokyo'
 
