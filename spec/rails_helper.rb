ec48f545ccd41905d9e4a2f84753096a03cf982a
everysick <s.wakeup31@gmail.com>
2016-04-10 14:50:13 +0900

    Add to load auth_logic in rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # sign_in sign_out logic
+  config.include AuthLogic
 end
