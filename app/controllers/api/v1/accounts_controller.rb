7a2f3b38f185eef5cc88a82a6c34c4938fe3014e
everysick <s.wakeup31@gmail.com>
2016-04-10 18:03:38 +0900

    Use refactored methods in account_controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -14,13 +14,15 @@ class Api::V1::AccountsController < ApplicationController
     ).symbolize_keys
 
     account = Account.create_with_kibokan(register_attributes)
+
     email = params[:kibokan][:email]
-    Accounts::RegisterEmailService.new(account, email).execute
+    Accounts::RegisterEmailService.new(account).execute(email)
 
     render json: { result: true }
   end
 
   def edit
+    current_account
     # return schema from mongodb
     # FIXME
     render json: { result: true }
@@ -28,11 +30,9 @@ class Api::V1::AccountsController < ApplicationController
 
   def email_confirm
     token = params[:email_set_token]
+    account = EmailRecoveryToken.find_by!(token: token).account
 
-    recovery_token = EmailRecoveryToken.find_by!(token: token)
-    account = recovery_token.account
-
-    Accounts::UpdateEmailService.new(account, recovery_token).execute
+    Accounts::UpdateEmailService.new(account).execute(token)
 
     sign_in!(account)
     render json: { result: true }
@@ -40,11 +40,11 @@ class Api::V1::AccountsController < ApplicationController
 
   def update
     if params[:email] != current_account.email
-      Accounts::RegisterEmailService.new(current_account, params[:email]).execute
+      Accounts::RegisterEmailService.new(current_account).execute(params[:email])
       params[:email] = current_account.email
     end
 
-    current_account.update_with_kibokan(params)
+    current_account.update_with_kibokan(params[:kibokan])
 
     render json: { result: true }
   end
