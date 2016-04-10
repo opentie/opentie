5bec798d03c94847aa624b38f4512a77a9734d06
everysick <s.wakeup31@gmail.com>
2016-04-10 13:52:31 +0900

    Impliment spec that test sessions_controller

--- spec/controllers/api/v1/auth/sessions_controller_spec.rb
+++ spec/controllers/api/v1/auth/sessions_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Auth::SessionsController, type: :controller do
+  describe "Sign in" do
+    before do
+      FactoryGirl.create(:account)
+      @params = FactoryGirl.attributes_for(:account).slice(:email, :password)
+
+      xhr :post, :sign_in, @params
+    end
+
+    it "Success sign_in" do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "Sign out" do
+    before do
+      FactoryGirl.create(:account)
+      @params = FactoryGirl.attributes_for(:account).slice(:email, :password)
+
+      xhr :post, :sign_in, @params
+    end
+
+    it "Success sign_in" do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "Success sign_out" do
+      xhr :post, :sign_out, {}
+
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+  end
 
 end
