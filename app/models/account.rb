9bb3862d6b7333dc01688f3e1c2ab044a1c21c23
everysick <s.wakeup31@gmail.com>
2016-04-10 17:13:19 +0900

    Add relationship that account has_many recovery_token

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,7 @@ class Account < ActiveRecord::Base
   validates :password             , length: { minimum: 8 }, if: :validate_password?
   validates :password_confirmation, presence: true        , if: :validate_password?
 
+  has_many :recovery_tokens
   has_many :email_recovery_tokens
   has_many :password_recovery_tokens
   has_many :roles
