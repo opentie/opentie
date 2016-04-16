e0cc7dd34cae7a65c5a67ef45eb366100cee848b
everysick <s.wakeup31@gmail.com>
2016-04-16 23:27:30 +0900

    Rename services

---
+++ app/services/update_email_service.rb
@@ -0,0 +1,18 @@
+class UpdateEmailService
+
+  attr_accessor :current_account
+
+  def initialize(current_account)
+    @current_account = current_account
+  end
+
+  def execute(token)
+    recovery_token =
+      current_account.email_recovery_tokens.find_by!(token: token)
+
+    new_email = recovery_token.email
+    current_account.update!(email: new_email)
+
+    recovery_token.disable
+  end
+end
---
+++ app/services/update_password_service.rb
@@ -0,0 +1,20 @@
+class UpdatePasswordService
+
+  attr_accessor :current_account
+
+  def initialize(current_account)
+    @current_account = current_account
+  end
+
+  def execute(token, password, password_confirmation)
+    recovery_token =
+      current_account.password_recovery_tokens.find_by!(token: token)
+
+    current_account.update!(
+      password: password,
+      password_confirmation: password_confirmation
+    )
+
+    recovery_token.disable
+  end
+end
