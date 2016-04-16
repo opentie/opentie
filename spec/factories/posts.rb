5b05116d1df34092be82612af23dcf170c5b52e5
everysick <s.wakeup31@gmail.com>
2016-04-16 21:36:15 +0900

    Create factory that create post, topic

---
+++ spec/factories/posts.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :post do
+    body "post-body"
+  end
+end
---
+++ spec/factories/topics.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :topic do
+    title "topic_name"
+    description "descriptions"
+  end
+end
