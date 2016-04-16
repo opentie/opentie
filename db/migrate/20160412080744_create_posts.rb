0605443a2c9f008d5fc31f9ee76edd20b3b47ceb
everysick <s.wakeup31@gmail.com>
2016-04-16 23:39:08 +0900

    Fix migration file

--- db/migrate/20160412080744_create_posts.rb
+++ db/migrate/20160412080744_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string     :body,                       default: ""
       t.uuid       :topic_id,       null: false
       t.uuid       :author_id,      null: false
+      t.uuid       :group_id,       null: false
       t.boolean    :is_draft,                   default: false
       t.timestamps                  null: false
     end
