fbf6b8c225da009b5db47264e851bbee6f5197f1
everysick <s.wakeup31@gmail.com>
2016-04-17 18:37:01 +0900

    Update sprc

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :models do
+
   let(:account) { FactoryGirl.create(:account) }
   let(:group) { FactoryGirl.create(:group) }
 
@@ -17,9 +18,24 @@ RSpec.describe Group, type: :models do
       end.to change { group.accounts.count }.by(1)
     end
 
+    it "has_many delegates dependent option" do
+      create_delegate
+      expect do
+        group.accounts.first.destroy
+      end.to change { group.delegates.count }.by(-1)
+    end
+
     it "has_many topics" do
       topic = create_topic(FactoryGirl.create(:division))
       expect do
+        GroupTopic.create(group: group, topic: topic)
+      end.to change { group.topics.count }.to(1)
+    end
+
+    it "has_many uniq topics" do
+      topic = create_topic(FactoryGirl.create(:division))
+      expect do
+        GroupTopic.create(group: group, topic: topic)
         GroupTopic.create(group: group, topic: topic)
       end.to change { group.topics.count }.to(1)
     end
@@ -28,13 +44,6 @@ RSpec.describe Group, type: :models do
       expect do
         create_topic(group)
       end.to change { group.proposal_topics.count }.by(1)
-    end
-
-    it "has_many posts" do
-      topic = create_topic(group)
-      expect do
-        create_post(topic)
-      end.to change { group.posts.count }.by(1)
     end
   end
 
@@ -55,7 +64,8 @@ RSpec.describe Group, type: :models do
   def create_topic(proposer)
     Topic.create(
       FactoryGirl.attributes_for(:topic).merge({
-        proposer: proposer
+        proposer: proposer,
+        account_id: account.id
       })
     )
   end
