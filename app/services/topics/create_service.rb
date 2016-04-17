ff8ba17e16f0841b0b6f92c1cfd87f9eb8c7fd73
everysick <s.wakeup31@gmail.com>
2016-04-17 21:08:29 +0900

    Ensure that author in not null when create topic

--- app/services/topics/create_service.rb
+++ app/services/topics/create_service.rb
@@ -7,12 +7,13 @@ module Topics
       @proposer = proposer
     end
 
-    def execute(params, group_ids)
+    def execute(params, author, group_ids)
       groups = Group.where(kibokan_id: group_ids)
 
       groups << @proposer if @proposer.class == Group
 
-      topic = @proposer.proposal_topics.new(params.merge({ groups: groups.uniq }))
+      topic = @proposer.proposal_topics.
+        new(params.merge({ groups: groups.uniq, author: author }))
       topic.sended_at = Time.now unless params[:is_draft]
 
       topic.save!
--- spec/services/topics/create_service_spec.rb
+++ spec/services/topics/create_service_spec.rb
@@ -6,13 +6,6 @@ module Topics
     let(:division) { FactoryGirl.create(:division) }
     let(:account) { FactoryGirl.create(:account) }
 
-    describe "initialize" do
-      it "new" do
-        service = new_topic(division)
-        expect(service.class).to eq(Topics::CreateService)
-      end
-    end
-
     describe "execute" do
       before do
         @groups = 10.times.map do |i|
@@ -34,18 +27,14 @@ module Topics
       end
     end
 
-    def new_topic(proposer)
-      Topics::CreateService.new(proposer)
-    end
-
     def create_topic(proposer, group_ids, is_draft)
       Topics::CreateService.new(proposer).execute(
         {
           title: "title",
           description: "des",
           is_draft: is_draft,
-          account: account
         },
+        account,
         group_ids
       )
     end
