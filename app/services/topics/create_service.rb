e5514125a17c0db19f0cc025777e831eb23cf3bc
everysick <s.wakeup31@gmail.com>
2016-04-20 15:53:28 +0900

    Ensure that update tag_list

--- app/services/topics/create_service.rb
+++ app/services/topics/create_service.rb
@@ -3,19 +3,27 @@ module Topics
 
     attr_accessor :proposer
 
-    def initialize(proposer)
-      @proposer = proposer
+    def initialize(proposer, account)
+      @proposer, @account = proposer, account
     end
 
-    def execute(params, author, is_draft, group_ids)
-      groups = Group.where(kibokan_id: group_ids).to_a
+    def execute(params)
+      is_draft = params.delete(:is_draft)
+      group_ids = params.delete(:group_ids)
+      tag_list = params.delete(:tag_list)
 
+      groups = Group.where(kibokan_id: group_ids).to_a
       groups << @proposer if @proposer.class == Group
 
       topic = @proposer.proposal_topics.
-        new(params.merge({ groups: groups.uniq, author: author }))
+        new(params.merge({ groups: groups.uniq, author: @account }))
 
-      topic.publish unless is_draft
+      topic.group_topics.each do |gt|
+        gt.tag_list = tag_list
+        gt.save
+      end
+
+      topic.publish! unless is_draft
       topic.save!
       topic
     end
--- app/services/topics/update_service.rb
+++ app/services/topics/update_service.rb
@@ -7,8 +7,12 @@ module Topics
       @topic = topic
     end
 
-    def execute(params, is_draft, group_ids)
-      topic.publish if !is_draft && topic.draft?
+    def execute(params)
+      is_draft = params.delete(:is_draft)
+      group_ids = params.delete(:group_ids)
+      tag_list = params.delete(:tag_list)
+
+      @topic.publish! if !is_draft && topic.draft?
       @topic.update!(params)
 
       new_groups = Group.where(kibokan_id: group_ids).to_a
@@ -17,6 +21,11 @@ module Topics
       common_groups = new_groups & @topic.groups
       @topic.group_topics.where.not(group: common_groups).destroy_all
       @topic.add_groups(new_groups - common_groups)
+
+      @topic.group_topics.each do |gt|
+        gt.tag_list = tag_list
+        gt.save
+      end
 
       @topic.save!
       @topic
