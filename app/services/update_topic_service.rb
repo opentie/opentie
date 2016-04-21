da2ac9c209d5a4b71e8a7cf341e1cce1c7ddd4e1
everysick <s.wakeup31@gmail.com>
2016-04-22 02:18:08 +0900

    Rename topic services

---
+++ app/services/update_topic_service.rb
@@ -0,0 +1,32 @@
+class UpdateTopicService
+
+  attr_accessor :topic
+
+  def initialize(topic)
+    @topic = topic
+  end
+
+  def execute(params)
+    is_draft = params.delete(:is_draft)
+    group_ids = params.delete(:group_ids)
+    tag_list = params.delete(:tag_list)
+
+    @topic.publish! if !is_draft && topic.draft?
+    @topic.update!(params)
+
+    new_groups = Group.where(kibokan_id: group_ids).to_a
+    new_groups << @topic.proposer if @topic.proposer.class == Group
+
+    common_groups = new_groups & @topic.groups
+    @topic.group_topics.where.not(group: common_groups).destroy_all
+    @topic.add_groups(new_groups - common_groups)
+
+    @topic.group_topics.each do |gt|
+      gt.tag_list = tag_list
+      gt.save
+    end
+
+    @topic.save!
+    @topic
+  end
+end
