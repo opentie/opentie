d53168feacd0b617ba1d198a3880f1bb82d37818
everysick <s.wakeup31@gmail.com>
2016-04-22 00:30:01 +0900

    Impliment divisions/topics_controller that temporary

--- app/controllers/api/v1/divisions/topics_controller.rb
+++ app/controllers/api/v1/divisions/topics_controller.rb
@@ -1,4 +1,83 @@
 module Api::V1::Divisions
   class TopicsController < Api::V1::Divisions::Topics::BaseController
+
+    before_action :division
+    before_action :tag,           except: [:new, :create]
+    before_action :post,          except: [:new, :create]
+    prepend_before_action :topic, except: [:new, :create]
+
+    def index
+      topics = Topic.published.all.to_a
+
+      render_ok({
+        topics: topics
+      })
+    end
+
+    def show
+      render_ok({
+        topic: @topic,
+        tags: @tags,
+        posts: @posts
+      })
+    end
+
+    def new
+      render_ok
+    end
+
+    def create
+      Topics::CreateService.
+        new(@division, current_account).
+        execute(project_params)
+
+      render_ok
+    end
+
+    def edit
+      render_ok({
+        topic: @topic,
+        tags: @tags,
+        posts: @posts
+      })
+    end
+
+    def update
+      Topics::UpdateService.
+        new(@topic).
+        execute(project_params)
+
+      render_ok
+    end
+
+    def destroy
+      @topic.destroy
+
+      render_ok
+    end
+
+    private
+
+    def tag
+      topic unless @topic
+
+      @tags = @topic.group_topics.map do |gt|
+        [ gt.group_id, gt.tag_list ]
+      end.to_h
+    end
+
+    def post
+      topic unless @topic
+
+      @posts = @topic.group_topics.map do |gt|
+        [ gt.group_id, gt.posts ]
+      end.to_h
+    end
+
+    def project_params
+      params.require(:topic).permit(
+        :title, :description, :is_draft, :group_ids, :tag_list
+      )
+    end
   end
 end
