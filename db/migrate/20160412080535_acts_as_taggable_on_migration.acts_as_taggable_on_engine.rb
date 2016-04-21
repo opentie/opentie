879612fe6a2dd6685bbc7262f99bb266f02172ef
everysick <s.wakeup31@gmail.com>
2016-04-22 00:27:56 +0900

    Change type of id from integer to uuid

--- db/migrate/20160412080535_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20160412080535_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -1,17 +1,23 @@
 # This migration comes from acts_as_taggable_on_engine (originally 1)
 class ActsAsTaggableOnMigration < ActiveRecord::Migration
   def self.up
-    create_table :tags do |t|
+    create_table :tags, id: :uuid do |t|
       t.string :name
     end
 
-    create_table :taggings do |t|
-      t.references :tag
+    create_table :taggings, id: :uuid do |t|
+      #t.references :tag
+      t.uuid :tag_id
 
       # You should make sure that the column created is
       # long enough to store the required class names.
-      t.references :taggable, polymorphic: true
-      t.references :tagger, polymorphic: true
+      #t.references :taggable, polymorphic: true
+      #t.references :tagger, polymorphic: true
+      t.uuid :taggable_id
+      t.string :taggable_type
+
+      t.uuid :tagger_id
+      t.string :tagger_type
 
       # Limit is created to prevent MySQL error on index
       # length for MyISAM table type: http://bit.ly/vgW2Ql
