51521432459afcea3d34caf7f21661e518414257
everysick <s.wakeup31@gmail.com>
2016-04-24 00:09:48 +0900

    Impliment base_controllers in group

--- app/controllers/api/v1/groups/base_controller.rb
+++ app/controllers/api/v1/groups/base_controller.rb
@@ -1,5 +1,17 @@
 module Api::V1
   class Groups::BaseController < Api::V1::BaseController
 
+    before_action :authenticate_account!
+    before_action :group
+
+    def group
+      unless @group
+        id = params[:group_id] || params[:id]
+        @group = Group.find(id)
+      end
+
+      ActiveRecord::RecordNotFound unless @group
+      @group
+    end
   end
 end
