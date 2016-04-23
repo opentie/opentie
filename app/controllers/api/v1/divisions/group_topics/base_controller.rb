261178109f688b3b0f5d8fcd616b83733ba374a3
everysick <s.wakeup31@gmail.com>
2016-04-23 18:57:05 +0900

    Ensure that load group_topic in top level controller

--- app/controllers/api/v1/divisions/group_topics/base_controller.rb
+++ app/controllers/api/v1/divisions/group_topics/base_controller.rb
@@ -1,6 +1,8 @@
 module Api::V1::Divisions
   class GroupTopics::BaseController < Api::V1::Divisions::BaseController
 
+    before_action :group_topic
+
     def group_topic
       unless @group_topic
         id = params[:group_topic_id] || params[:id]
