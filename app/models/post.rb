3204ad0a372f28d9cf110f00f7a9dbfd77b19cc3
everysick <s.wakeup31@gmail.com>
2016-04-23 19:32:00 +0900

    Impliment draft manager in models/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,20 @@ class Post < ActiveRecord::Base
 
   belongs_to :author, class_name: 'Account'
   belongs_to :group_topic
+
+  scope :draft, -> { where(is_draft: true) }
+  scope :published, -> { where(is_draft: false) }
+
+  def publish!
+    return if published?
+    self.update(is_draft: false, sended_at: Time.now)
+  end
+
+  def published?
+    !is_draft && !sended_at.nil?
+  end
+
+  def draft?
+    is_draft
+  end
 end
