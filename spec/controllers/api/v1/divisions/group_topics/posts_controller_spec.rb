2cbefaa225bf6956a946e40feeb22880265b8cc9
everysick <s.wakeup31@gmail.com>
2016-04-23 17:22:32 +0900

    Replace spec files that test group_topics_controllers

---
+++ spec/controllers/api/v1/divisions/group_topics/posts_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Api::V1::Divisions::GroupTopics
+  RSpec.describe PostsController, type: :controller do
+
+  end
+end
---
+++ spec/controllers/api/v1/divisions/group_topics_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Api::V1::Divisions::GroupTopics
+  RSpec.describe GroupTopicsController, type: :controller do
+
+  end
+end
