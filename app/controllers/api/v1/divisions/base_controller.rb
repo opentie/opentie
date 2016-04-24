822c87c7d113ab140fc1d502e2d41a85005ca96a
everysick <s.wakeup31@gmail.com>
2016-04-24 19:23:18 +0900

    Ensure that load role in /api/v1/divisions/*

--- app/controllers/api/v1/divisions/base_controller.rb
+++ app/controllers/api/v1/divisions/base_controller.rb
@@ -3,15 +3,27 @@ module Api::V1
 
     before_action :authenticate_account!
     before_action :division
+    before_action :role
 
     def division
       unless @division
         id = params[:division_id] || params[:id]
-        @division = Division.find(id)
+        @division =current_account.divisions.includes(
+          :roles, :accounts
+        ).find(id)
       end
 
-      ActiveRecord::RecordNotFound unless @division
+      raise ActiveRecord::RecordNotFound unless @division
       @division
+    end
+
+    def role
+      unless @role
+        @role = @division.roles.find_by(account: current_account)
+      end
+
+      raise ActiveRecord::RecordNotFound unless @role
+      @role
     end
   end
 end
