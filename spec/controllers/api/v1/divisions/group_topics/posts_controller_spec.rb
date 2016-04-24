5dc1e5af8cf30ccfb1e25fb1aef42cbca02e866c
everysick <s.wakeup31@gmail.com>
2016-04-24 21:11:01 +0900

    Fix spec based on application routing

--- spec/controllers/api/v1/divisions/group_topics/posts_controller_spec.rb
+++ spec/controllers/api/v1/divisions/group_topics/posts_controller_spec.rb
@@ -4,16 +4,19 @@ module Api::V1::Divisions::GroupTopics
   RSpec.describe PostsController, type: :controller do
     let(:account) { FactoryGirl.create(:account) }
     let(:division) { FactoryGirl.create(:division) }
-    let(:group_topic) { GroupTopic.first }
+    let(:group) { FactoryGirl.create(:group) }
+    let(:topic) { Topic.first }
 
     before do
+      group.delegates.create(account: account, permission: 'normal')
       division.roles.create(account: account, permission: 'normal')
+      @group_topic = topic.group_topics.create(group: group)
+      sign_in!(account)
     end
 
-    describe "GET /divisions/:division_id/group_topics/:group_topic_id/posts/draft" do
+    describe "GET /divisions/:division_id/group_topics/:group_topic_id/posts" do
       before do
-        sign_in!(account)
-        xhr :get, :draft, id_params
+        xhr :get, :index, id_params
       end
 
       it "200 OK" do
@@ -27,9 +30,9 @@ module Api::V1::Divisions::GroupTopics
       end
     end
 
+
     describe "GET /divisions/:division_id/group_topics/:group_topic_id/posts/new" do
       before do
-        sign_in!(account)
         xhr :get, :new, id_params
       end
 
@@ -39,10 +42,11 @@ module Api::V1::Divisions::GroupTopics
       end
     end
 
+
     describe "POST /divisions/:division_id/group_topics/:group_topic_id/posts" do
       before do
-        sign_in!(account)
-        xhr :post, :create, id_params.merge(post_params)
+        params = { post: FactoryGirl.attributes_for(:post) }
+        xhr :post, :create, id_params.merge(params)
       end
 
       it "200 OK" do
@@ -51,10 +55,11 @@ module Api::V1::Divisions::GroupTopics
       end
     end
 
+
     describe "GET /divisions/:division_id/group_topics/:group_topic_id/posts/edit/:id" do
       before do
-        sign_in!(account)
-        xhr :get, :edit, id_params.merge({ id: group_topic.posts.first.id })
+        post = create_post
+        xhr :get, :edit, id_params.merge({ id: post.id })
       end
 
       it "200 OK" do
@@ -64,16 +69,21 @@ module Api::V1::Divisions::GroupTopics
 
       it "has attributes" do
         body = JSON.parse(response.body).deep_symbolize_keys
-        expect(body.include?(:post)).to eq(true)
+        expect(body.include?(:body)).to eq(true)
       end
     end
 
+
     describe "PUT /divisions/:division_id/group_topics/:group_topic_id/posts/:id" do
       before do
-        sign_in!(account)
+        post = create_post
 
-        params = id_params.merge(post_params).merge({ id: group_topic.posts.first.id })
-        xhr :put, :update, params
+        params = {
+          post: FactoryGirl.attributes_for(:post),
+          id: post.id
+        }
+
+        xhr :put, :update, id_params.merge(params)
       end
 
       it "200 OK" do
@@ -82,15 +92,22 @@ module Api::V1::Divisions::GroupTopics
       end
     end
 
+
     def id_params
       {
         division_id: division.id,
-        group_topic_id: group_topic.id
+        group_topic_id: @group_topic.id
       }
     end
 
-    def post_params
-      { post: FactoryGirl.attributes_for(:post) }
+    def create_post
+      post = @group_topic.posts.create(
+        body: "new-body",
+        author: account,
+        is_draft: true,
+        division: division
+      )
+      post
     end
   end
 end
