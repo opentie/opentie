e448056424d0f73d7624e4c23e78e927e3608914
everysick <s.wakeup31@gmail.com>
2016-04-28 22:09:34 +0900

    Impliment groups_controller.rb with spec

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,5 +1,55 @@
 module Api::V1
   class GroupsController < Api::V1::Groups::BaseController
 
+    before_action :authenticate_account!, only: [:show]
+    before_action :group,     except: [:new, :create]
+    before_action :category,  except: [:new, :create]
+
+    def new
+      category_name = params[:category_name]
+      form = Group.get_root_form(category_name)
+
+      render_ok({
+        form: form
+      })
+    end
+
+    def create
+      Group.create_with_kibokan(group_params)
+
+      render_created
+    end
+
+    def edit
+      entity = @group.get_entity
+
+      render_ok({
+        group: group.attributes.merge(entity.attributes)
+      })
+    end
+
+    def update
+      @group.update_with_kibokan(group_params)
+
+      render_ok
+    end
+
+    def show
+      entity = @group.get_entity
+
+      render_ok({
+        group: @group.attributes.merge(entity.attributes)
+      })
+    end
+
+    private
+
+    def group_params
+      params.require(:group).permit(
+        :kibokan
+      ).merge({
+        category_name: params[:category_name]
+      })
+    end
   end
 end
--- spec/controllers/api/v1/groups_controller_spec.rb
+++ spec/controllers/api/v1/groups_controller_spec.rb
@@ -2,6 +2,104 @@ require 'rails_helper'
 
 module Api::V1
   RSpec.describe GroupsController, type: :controller do
+    let(:account) { FactoryGirl.create(:account) }
+    let(:group) { FactoryGirl.create(:group) }
 
+    describe "GET /api/v1/groups/new" do
+      before do
+        params = {
+          category_name: "name"
+        }
+        xhr :get, :new, params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:form)).to eq(true)
+      end
+    end
+
+    describe "POST /api/v1/groups/" do
+      before do
+        @params = {
+          kibokan: {},
+          category_name: "category_name",
+          group: FactoryGirl.attributes_for(:group)
+        }
+      end
+
+      it '201 OK' do
+        xhr :post, :create, @params
+        expect(response).to be_success
+        expect(response.status).to eq(201)
+      end
+    end
+
+    describe "GET /api/v1/groups/edit" do
+      before do
+        Delegate.create(group: group, account: account)
+        sign_in!(account)
+        params = {
+          id: group.id,
+        }
+        xhr :get, :edit, params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:group)).to eq(true)
+      end
+    end
+
+    describe "PUT /api/v1/groups/:id" do
+      before do
+        Delegate.create(group: group, account: account)
+        sign_in!(account)
+        params = {
+          id: group.id,
+          category_name: "category_name",
+          group: FactoryGirl.attributes_for(:group)
+        }
+        xhr :put, :update, params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "GET /api/v1/groups/:id" do
+      before do
+        Delegate.create(group: group, account: account)
+        sign_in!(account)
+        params = {
+          id: group.id,
+          category_name: "category_name",
+          kibokan: {}
+        }
+        xhr :get, :show, params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:group)).to eq(true)
+      end
+    end
   end
 end
