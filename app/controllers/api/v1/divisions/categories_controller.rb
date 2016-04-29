ca856d63402de3b3e8ffd92cca2493ff832cb2c0
everysick <s.wakeup31@gmail.com>
2016-04-30 04:49:11 +0900

    Impliment categories_controller.rb

--- app/controllers/api/v1/divisions/categories_controller.rb
+++ app/controllers/api/v1/divisions/categories_controller.rb
@@ -1,5 +1,48 @@
 module Api::V1::Divisions
   class CategoriesController < Api::V1::Divisions::Categories::BaseController
 
+    before_action :category, except: [:index, :new, :create]
+
+    def index
+      categories = Category.all(Group.current_namespace)
+
+      render_ok({
+        categories: categories
+      })
+    end
+
+    def show
+      render_ok({
+        category: @category
+      })
+    end
+
+    def new
+      render_ok
+    end
+
+    def create
+      Category.create(Group.current_namespace, params)
+      render_created
+    end
+
+    def edit
+      render_ok({
+        category: @category
+      })
+    end
+
+    def update
+      @category.update(category_params)
+      render_ok
+    end
+
+    private
+
+    def category_params
+      params.permit(
+        :kibokan
+      )
+    end
   end
 end
--- spec/controllers/api/v1/divisions/categories_controller_spec.rb
+++ spec/controllers/api/v1/divisions/categories_controller_spec.rb
@@ -2,6 +2,113 @@ require 'rails_helper'
 
 module Api::V1::Divisions::Categories
   RSpec.describe CategoriesController, type: :controller do
+    let(:account) { FactoryGirl.create(:account) }
+    let(:division) { FactoryGirl.create(:division) }
 
+    before do
+      division.roles.create(account: account)
+      sign_in!(account)
+    end
+
+    describe 'GET /api/v1/divisions/:division_id/categories/' do
+      before do
+        xhr :get, :index, default_params
+      end
+
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:categories)).to eq(true)
+      end
+    end
+
+    describe 'GET /api/v1/divisions/:division_id/categories/:id' do
+      before do
+        xhr :get, :show, category_params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:category)).to eq(true)
+      end
+    end
+
+    describe 'GET /api/v1/divisions/:division_id/categories/new' do
+      before do
+        xhr :get, :new, category_params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'POST /api/v1/divisions/:division_id/categories/' do
+      before do
+        xhr :post, :create, store_params
+      end
+
+      it '201 Created' do
+        expect(response).to be_success
+        expect(response.status).to eq(201)
+      end
+    end
+
+    describe 'GET /api/v1/divisions/:division_id/categories/edit' do
+      before do
+        xhr :get, :edit, category_params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+        expect(body.include?(:category)).to eq(true)
+      end
+    end
+
+    describe 'PUT /api/v1/divisions/:division_id/categories/:id' do
+      before do
+        xhr :put, :update, store_params.merge(category_params)
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+    end
+
+
+    def default_params
+      {
+        division_id: division.id,
+      }
+    end
+
+    def store_params
+      default_params.merge({
+        kibokan: {}
+      })
+    end
+
+    def category_params
+      default_params.merge({
+        name: 'category_name'
+      })
+    end
   end
 end
