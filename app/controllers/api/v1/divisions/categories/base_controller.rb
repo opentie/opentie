ad6b735cf01927b047a2a16b316741394ae29691
everysick <s.wakeup31@gmail.com>
2016-04-30 03:15:28 +0900

    Ensure that auto load category when request

--- app/controllers/api/v1/divisions/categories/base_controller.rb
+++ app/controllers/api/v1/divisions/categories/base_controller.rb
@@ -1,5 +1,15 @@
 module Api::V1::Divisions
   class Categories::BaseController < Api::V1::Divisions::BaseController
 
+    before_action :category
+
+    def category
+      unless @category
+        @category = params[:category_name] || params[:name]
+      end
+
+      ActiveRecord::RecordNotFound unless @category
+      @category
+    end
   end
 end
