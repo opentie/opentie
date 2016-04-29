27ed5b2b7b6d42ae4e1c2bca567ed4bbf4c3b01a
everysick <s.wakeup31@gmail.com>
2016-04-30 01:03:17 +0900

    Refactor category model with spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,27 +1,75 @@
 class Category
   include Virtus.model
+  include ActiveModel::Validations
 
   attribute :namespace, String
   attribute :name, String
+  attribute :payload, Hash
 
-  def payload
-    path = self.class.request_path + "#{self.name}"
-    Kibokan::Agent.new(path).get
+  validates :name, :namespace, presence: true
+
+  def initialize(*params)
+    super
+    self.payload = get_payload
+    self
   end
 
   def self.all(namespace)
-    path = "namespaces/#{namespace}/categories"
-    categories = Kibokan::Agent.new(path).get
+    path = request_path(namespace)
+    categories = Kibokan::RequestAgent.new(path).index
+
     categories.map do |c|
       Category.new(
         namespace: namespace,
-        name: c[:_name],
-        payload: c
+        name: c[:_name]
       )
     end
   end
 
-  def self.request_path(namepsace)
+  def self.request_path(namespace)
     "namespaces/#{namespace}/categories"
+  end
+
+  def get_root_form
+    path = Form.request_path(self.namespace, self.name)
+
+    form_body = Kibokan::RequestAgent.new(path).new
+    Form.from_kibokan(form_body)
+  end
+
+  def create_form(params)
+    path = Form.request_path(self.namespace, self.name)
+
+    form_body = Kibokan::RequestAgent.new(path).create(params)
+    Form.from_kibokan(form_body)
+  end
+
+  def get_form(form_id)
+    path = Form.request_path(self.namespace, self.name) + "/#{form_id}"
+
+    form_body = Kibokan::RequestAgent.new(path).show
+    Form.from_kibokan(form_body)
+  end
+
+  def get_forms(form_ids=nil)
+    path = Form.request_path(self.namespace, self.name)
+
+    form_bodies = nil
+    if form_ids.nil?
+      form_bodies = Kibokan::RequestAgent.new(path).index
+    else
+      form_bodies = Kibokan::RequestAgent.new(path).bulk({ ids: form_ids })
+    end
+
+    form_bodies.map do |form_body|
+      Form.from_kibokan(form_body)
+    end
+  end
+
+  private
+
+  def get_payload
+    path = self.class.request_path(self.namespace) + "/#{self.name}"
+    Kibokan::RequestAgent.new(path).show
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,28 +2,71 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :models do
 
-  describe 'initialize' do
-    it 'success' do
-      category = Category.new(namespace: 'namespace', name: 'name')
-      expect(category.class).to eq(Category)
+  describe 'initializer' do
+    context 'valid params' do
+      it 'valid' do
+        category = Category.new(namespace: 'namespace', name: 'name')
+        expect(category.valid?).to eq(true)
+      end
     end
 
-    it 'failure' do
-      category = Category.new(name: 'name')
-      expect(category.valid?).to eq(false)
+    context 'invalid params' do
+      it 'be invalid' do
+        category = Category.new(name: 'name')
+        expect(category.valid?).to eq(false)
 
-      category = Category.new(namespace: 'namespace')
-      expect(category.valid?).to eq(false)
+        category = Category.new(namespace: 'namespace')
+        expect(category.valid?).to eq(false)
+      end
+    end
+
+    it 'has payload' do
+      category = Category.new(namespace: 'namespace', name: 'name')
+      expect(category.payload).not_to eq(nil)
     end
   end
 
   describe 'instance methods' do
-    it 'good payload' do
-      category = Category.new(name: 'name', namespace: 'namespace')
+    before do
+      @category = Category.new(namespace: 'namespace', name: 'name')
+    end
 
-      payload = category.payload
-      expect(payload.include?(:_id)).to eq(true)
-      expect(payload.include?(:_name)).to eq(true)
+    describe '#root_form' do
+      it 'return sample form' do
+        form = @category.get_root_form
+        expect(form.class).to eq(Form)
+      end
+    end
+
+    describe '#create_form' do
+      it 'return sample form' do
+        form = @category.create_form({})
+        expect(form.class).to eq(Form)
+      end
+    end
+
+    describe '#get_form' do
+      it 'return sample form' do
+        form = @category.get_form('id')
+        expect(form.class).to eq(Form)
+      end
+    end
+
+    describe '#get_forms' do
+      context 'with blank form_ids' do
+        it 'return sample forms' do
+          forms = @category.get_forms
+          expect(forms.class).to eq(Array)
+          expect(forms.first.class).to eq(Form)
+        end
+      end
+      context 'with blank form_ids' do
+        it 'return sample forms' do
+          forms = @category.get_forms(['id'])
+          expect(forms.class).to eq(Array)
+          expect(forms.first.class).to eq(Form)
+        end
+      end
     end
   end
 
