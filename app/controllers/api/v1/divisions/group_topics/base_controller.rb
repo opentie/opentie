be70ccfcb0fc43175b96a6b3462259f492663d31
everysick <s.wakeup31@gmail.com>
2016-04-30 03:15:01 +0900

    Ensure that auto load group and category when request

--- app/controllers/api/v1/divisions/group_topics/base_controller.rb
+++ app/controllers/api/v1/divisions/group_topics/base_controller.rb
@@ -2,6 +2,8 @@ module Api::V1::Divisions
   class GroupTopics::BaseController < Api::V1::Divisions::BaseController
 
     before_action :group_topic
+    before_action :group
+    before_action :category
 
     def group_topic
       unless @group_topic
@@ -11,6 +13,24 @@ module Api::V1::Divisions
 
       ActiveRecord::RecordNotFound unless @group_topic
       @group_topic
+    end
+
+    def group
+      unless @group
+        @group = @group_topic.group
+      end
+
+      ActiveRecord::RecordNotFound unless @group
+      @group
+    end
+
+    def category
+      unless @category
+        @category = @group.category_name
+      end
+
+      ActiveRecord::RecordNotFound unless @category
+      @category
     end
   end
 end
