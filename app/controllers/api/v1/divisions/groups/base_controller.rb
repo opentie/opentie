407ab8aa2ea4260167b7f9568cfac67ca7aa5087
everysick <s.wakeup31@gmail.com>
2016-05-01 07:42:16 +0900

    Ensure that load group before action in Api::V1::Divisions::Groups::

--- app/controllers/api/v1/divisions/groups/base_controller.rb
+++ app/controllers/api/v1/divisions/groups/base_controller.rb
@@ -1,5 +1,16 @@
 module Api::V1::Divisions
   class Groups::BaseController < Api::V1::Divisions::BaseController
 
+    before_action :group
+
+    def group
+      unless @group
+        id = params[:group_id] || params[:id]
+        @group = Group.find(id)
+      end
+
+      raise ActiveRecord::RecordNotFound unless @group
+      @group
+    end
   end
 end
