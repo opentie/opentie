cfe344034a4192f11dedc478b7b1c2dc213a5776
everysick <s.wakeup31@gmail.com>
2016-05-01 12:35:19 +0900

    Delete unneccessary file and line

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,6 @@ Category.create('accounts', {
   forms: []
 })
 
-json_raw = File.read("#{Rails.root}/config/initial_category.json")
-category_hash = JSON.parse(json_raw)
-
-Category.create('groups', category_hash)
-
 unless Rails.env.production?
 
   Category.create('groups', {
