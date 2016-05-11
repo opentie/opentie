b8cad4aa00869ffc558eeba9cace3183c12b9cf5
everysick <s.wakeup31@gmail.com>
2016-05-11 18:17:19 +0900

    Implement groups_controller#index that return groups in each category

--- app/controllers/api/v1/divisions/categories/groups_controller.rb
+++ app/controllers/api/v1/divisions/categories/groups_controller.rb
@@ -1,5 +1,16 @@
 module Api::V1::Divisions::Categories
   class GroupsController < Api::V1::Divisions::Categories::BaseController
 
+    before_action :category
+
+    def index
+      groups = Group.get_entities(@category.name).map do |entity|
+        entity.payload
+      end
+
+      render_ok({
+        groups: groups
+      })
+    end
   end
 end
--- spec/controllers/api/v1/divisions/categories/groups_controller_spec.rb
+++ spec/controllers/api/v1/divisions/categories/groups_controller_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 module Api::V1::Divisions::Categories
   RSpec.describe GroupsController, type: :controller do
+    let(:account) { FactoryGirl.create(:account) }
+    let(:division) { Division.first }
 
+
+    describe 'GET /api/v1/divisions/:division_id/categories/:category_name/groups' do
+      before do
+        division.roles.create(account: account, permission: 'normal')
+        sign_in!(account)
+        xhr :get, :index, default_params
+      end
+
+      it '200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'has attributes' do
+        body = JSON.parse(response.body).deep_symbolize_keys
+
+        expect(body.include?(:groups)).to eq(true)
+        expect(body[:groups].class).to eq(Array)
+      end
+    end
+
+    def default_params
+      {
+        division_id: division.id,
+        category_name: Group.first.category_name
+      }
+    end
   end
 end
